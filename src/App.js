eb5123cb4a8b6cb6edeacda73246ae2bed6ca709
XandrKhv <starnetkhv@gmail.com>
2018-07-08 11:13:16 +1000

    Сверстали макет, используя компоненты React

--- src/App.js
+++ src/App.js
@@ -9,25 +9,49 @@ import Header from './components/Header'
 import Menu from './components/Menu'
 import Main from './components/Main'
 
+const url = process.env.PUBLIC_URL + '/img/bg.jpg'
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
+
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <SayFullName name="Kate" surname="Bubnovskaya" link="vk.com" />
-        <SayFullName name="Elena" surname="Petrova" link="faxebook.com" />
-        <SayFullName name="Max" surname="Serov" link="max-factor.com" />
+      <div className="App">
+      <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
       </div>
     )
   }
-}
-
-function SayFullName(props) {
-  return (
-      <div>
-        <h1> Мое имя {props.name} , фамилия - {props.surname}</h1>
-        <a href="{props.link}"> Ссылка на мой профиль </a>
-      </div>
-    )
 }
 
 export default App;
